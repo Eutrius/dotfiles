061132e76c4dc696eab4b3a353b512899fd99c09
eutrius <eutrius@gmail.com>
2025-07-20 21:23:06 +0200

    refactor: minimalize status line, add tmux-resurrect

--- set_terminal.sh
+++ set_terminal.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
 if [ "$TERM" = "xterm-kitty" ]; then
-  tmux set -g default-terminal "xterm-kitty"
+	tmux set -g default-terminal "xterm-kitty"
+	tmux set -as terminal-overrides ",xterm-kitty:Tc"
 else
-  tmux set -g default-terminal "xterm-256color"
+	tmux set -g default-terminal "xterm-256color"
+	tmux set -as terminal-overrides ",xterm-256color:Tc"
 fi
