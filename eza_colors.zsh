fe603a31fedf95a195adb2464afcbee9c41470a3
eutrius <eutrius@gmail.com>
2025-12-06 14:43:13 +0100

    style(eza): standardize section comments and casing

--- eza_colors.zsh
+++ eza_colors.zsh
@@ -1,5 +1,3 @@
-# Eza Color Configuration
-
 # File types
 export EZA_COLORS="di=34:fi=97:ex=31:ln=36"
 
@@ -22,7 +20,7 @@ export EZA_COLORS="${EZA_COLORS}:bd=38;5;25:cd=38;5;25:so=38;5;25:pi=38;5;25"
 # Git status
 export EZA_COLORS="${EZA_COLORS}:ga=38;5;32:gm=38;5;220:gd=38;5;160:gv=38;5;37:gt=38;5;37"
 
-# Programming Languages
+# Programming languages
 export EZA_COLORS="${EZA_COLORS}:*.py=38;5;32:*.pyi=38;5;32:*.pyc=38;5;67"
 export EZA_COLORS="${EZA_COLORS}:*.js=38;5;32:*.ts=38;5;32:*.jsx=38;5;32:*.tsx=38;5;32"
 export EZA_COLORS="${EZA_COLORS}:*.cpp=38;5;32:*.cc=38;5;32:*.cxx=38;5;32:*.c=38;5;32:*.h=38;5;32:*.hpp=38;5;32:*.hxx=38;5;32"
@@ -30,19 +28,19 @@ export EZA_COLORS="${EZA_COLORS}:*.rb=38;5;32:*.php=38;5;32:*.java=38;5;32:*.go=
 export EZA_COLORS="${EZA_COLORS}:*.scala=38;5;32:*.kt=38;5;32:*.swift=38;5;32:*.dart=38;5;32"
 export EZA_COLORS="${EZA_COLORS}:*.lua=38;5;32:*.pl=38;5;32:*.sh=38;5;32:*.bash=38;5;32:*.zsh=38;5;32"
 
-# Web Development
+# Web development
 export EZA_COLORS="${EZA_COLORS}:*.html=38;5;37:*.htm=38;5;37:*.css=38;5;37:*.scss=38;5;37:*.sass=38;5;37:*.less=38;5;37"
 export EZA_COLORS="${EZA_COLORS}:*.svg=38;5;37:*.vue=38;5;37:*.wasm=38;5;37"
 
-# Data and Config files
+# Data and config files
 export EZA_COLORS="${EZA_COLORS}:*.json=38;5;37:*.yaml=38;5;37:*.yml=38;5;37:*.toml=38;5;37:*.ini=38;5;37"
 export EZA_COLORS="${EZA_COLORS}:*.xml=38;5;37:*.csv=38;5;37:*.tsv=38;5;37"
 
-# Build and Package files
+# Build and package files
 export EZA_COLORS="${EZA_COLORS}:Makefile=38;5;25:*.mk=38;5;25:*.cmake=38;5;25"
 export EZA_COLORS="${EZA_COLORS}:*.lock=38;5;67:*.pkg=38;5;67:*.deb=38;5;67:*.rpm=38;5;67"
 
-# Documents and Text
+# Documents and text
 export EZA_COLORS="${EZA_COLORS}:*.md=38;5;32:*.txt=38;5;159:*.log=38;5;67:*.pdf=38;5;25"
 export EZA_COLORS="${EZA_COLORS}:*.doc=38;5;25:*.docx=38;5;25:*.odt=38;5;25"
 export EZA_COLORS="${EZA_COLORS}:*.xls=38;5;25:*.xlsx=38;5;25:*.ods=38;5;25"
@@ -56,7 +54,7 @@ export EZA_COLORS="${EZA_COLORS}:*.mp4=38;5;37:*.avi=38;5;37:*.mkv=38;5;37:*.mov
 # Archives
 export EZA_COLORS="${EZA_COLORS}:*.zip=38;5;25:*.tar=38;5;25:*.gz=38;5;25:*.bz2=38;5;25:*.xz=38;5;25:*.rar=38;5;25:*.7z=38;5;25"
 
-# Temporary and System files
+# Temporary and system files
 export EZA_COLORS="${EZA_COLORS}:*.tmp=38;5;67:*.swp=38;5;67:*.swo=38;5;67:*.swn=38;5;67:*.bak=38;5;67"
 export EZA_COLORS="${EZA_COLORS}:*.pid=38;5;67:*.sock=38;5;67"
 
