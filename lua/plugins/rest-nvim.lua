e23b297257147faa0077a2ee8f9e7d533e5c3f8a
eutrius <eutrius@gmail.com>
2026-07-17 10:46:28 +0200

    refactor(rest): drop luarocks.nvim for lazy native rocks

--- lua/plugins/rest-nvim.lua
+++ lua/plugins/rest-nvim.lua
@@ -1,43 +1,27 @@
 return {
-	{
-		"vhyrro/luarocks.nvim",
-		event = "VeryLazy",
-		priority = 1000,
-		config = true,
-		opts = {
-			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-		},
-	},
-	{
-		"rest-nvim/rest.nvim",
-		ft = "http",
-		dependencies = { "luarocks.nvim" },
-		config = function()
-            require("rest-nvim").setup({
-                result = {
-                    show_curl_command = false,
-                    show_http_info = true,
-                    show_headers = true,
-                },
-                highlight = {
-                    enabled = true,
-                    timeout = 150,
-                },
-                jump_to_request = false,
-
-                -- IMPORTANT
-                skip_ssl_verification = false,
-                encode_url = true,
-                logs = {
-                    level = "info",
-                },
-
-                -- THIS ENABLES COOKIE STORAGE
-                cookies = {
-                    enable = true,
-                    file_name = ".rest_cookies.json",
-                },
-            })
-		end,
-	},
+	"rest-nvim/rest.nvim",
+	ft = "http",
+	config = function()
+		require("rest-nvim").setup({
+			result = {
+				show_curl_command = false,
+				show_http_info = true,
+				show_headers = true,
+			},
+			highlight = {
+				enabled = true,
+				timeout = 150,
+			},
+			jump_to_request = false,
+			skip_ssl_verification = false,
+			encode_url = true,
+			logs = {
+				level = "info",
+			},
+			cookies = {
+				enable = true,
+				file_name = ".rest_cookies.json",
+			},
+		})
+	end,
 }
