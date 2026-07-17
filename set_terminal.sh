dd8026af0dbc15246e023f79caab60cdea4462d1
eutrius <eutrius@gmail.com>
2026-07-17 09:13:29 +0200

    fix(tmux): set default-terminal to tmux-256color

--- set_terminal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$TERM" = "xterm-kitty" ]; then
-	tmux set -g default-terminal "xterm-kitty"
-	tmux set -as terminal-overrides ",xterm-kitty:Tc"
-else
-	tmux set -g default-terminal "xterm-256color"
-	tmux set -as terminal-overrides ",xterm-256color:Tc"
-fi
